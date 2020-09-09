d23cf8241364466b0b1c3665d61111a7f8357151
Grzegorz Kuliński <kulinski.grzegorz90@gmail.com>
2020-09-09 17:17:38 +0200

    spring initial project

---
+++ src/main/java/pl/gkulinski/happypictures/HappyPicturesApplication.java
@@ -0,0 +1,13 @@
+package pl.gkulinski.happypictures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyPicturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyPicturesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/gkulinski/happypictures/HappyPicturesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gkulinski.happypictures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyPicturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
